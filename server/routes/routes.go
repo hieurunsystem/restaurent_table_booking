f9374a1d1360335c621b5441a7562a8c9ff59d24
hieurunsystem <hieuln1@runsystem.net>
2025-03-14 14:37:40 +0700

    create restaurant & get all restaurents

--- server/routes/routes.go
+++ server/routes/routes.go
@@ -3,7 +3,12 @@ package routes
 import "github.com/gin-gonic/gin"
 
 func Routes(server *gin.Engine) {
+	// User Management
 	server.GET("/", Home)
 	server.POST("/login", Login)
 	server.POST("/register", Register)
+
+	// Restaurant Management
+	server.GET("/restaurants/", GetAllRestaurants)
+	server.POST("/restaurants/create", CreateRestaurant)
 }
